b13244fc7dcd33c7fe78fef211eb12064f609201
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-17 00:07:28 +0100

    Require the http request

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+Primary file for the APi
+*/
+
+//Dependencies
+
+var http = require('http')
+
+//The server should respond to all request with a string
+
+
+//Start the server and let it listen on port 3000
