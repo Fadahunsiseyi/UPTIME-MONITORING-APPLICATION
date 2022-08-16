302dad311468f39a666bdb9c3194173aa239127a
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-17 00:32:21 +0100

    Server listening to a port

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ var server = http.createServer((req,res) => {
     res.end('Hello world\n');
 })
 
-
 //Start the server and let it listen on port 3000
+
+server.listen(3000, () => {
+    console.log('The server is listening on port 3000')
+})
