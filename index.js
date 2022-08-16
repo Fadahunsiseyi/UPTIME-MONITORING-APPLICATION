5893faa203d748476222792a92666c96464d34a5
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-17 00:26:35 +0100

    Creat the server with the http request and the createServer method

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ var http = require('http')
 
 //The server should respond to all request with a string
 
+var server = http.createServer((req,res) => {
+    res.end('Hello world\n');
+})
+
 
 //Start the server and let it listen on port 3000
