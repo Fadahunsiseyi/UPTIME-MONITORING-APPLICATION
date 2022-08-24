f891c683d9886cf0dff80b579c6239bbbdfc81d8
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-24 23:15:22 +0100

    Fix linters

--- index.js
+++ index.js
@@ -4,36 +4,34 @@ Primary file for the API
 
 //Dependencies
 
-var http = require('http')
-var url = require('url');
+var http = require("http");
+var url = require("url");
 
 //The server should respond to all request with a string
 
-var server = http.createServer((req,res) => {
+var server = http.createServer((req, res) => {
+  //Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  // console.log(parsedUrl);
 
-    //Get the url and parse it
-    const parsedUrl = url.parse(req.url, true);
-    // console.log(parsedUrl);
+  //Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  //Get query string as an object
+  const queryStringObject = parsedUrl.query;
+  var method = req.method.toLowerCase();
 
-    //Get the path
-    var path = parsedUrl.pathname
-    var trimmedPath = path.replace(/^\/+|\/+$/g,'')
-    var method = req.method.toLowerCase()
+  //Get the response
+  res.end("Hello world\n");
 
-    //Get query string from a url
-    const queryStringObject = parsedUrl.query
-    console.log(queryStringObject,'line 25')
-
-    //Get the response
-    res.end('Hello world\n');
-
-    //Log the request path
-    console.log('Request is received on path: ' + trimmedPath + ' on method: '+ method);
-
-})
+  //Log the request path
+  console.log(
+    "Request is received on path: " + trimmedPath + " on method: " + method + queryStringObject
+  );
+});
 
 //Start the server and let it listen on port 3000
 
 server.listen(3000, () => {
-    console.log('The server is listening on port 3000')
-})
+  console.log("The server is listening on port 3000");
+});
