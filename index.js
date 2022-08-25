315ba55cb30317fe1626093b756719f9f82c347e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-25 13:48:00 +0100

    Pass payload if any, based on the req event

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Primary file for the API
 
 var http = require("http");
 var url = require("url");
-var StringDecoder = require('string_decoder').StringDecoder;
+var StringDecoder = require("string_decoder").StringDecoder;
 
 //The server should respond to all request with a string
 
@@ -22,20 +22,26 @@ var server = http.createServer((req, res) => {
   const queryStringObject = parsedUrl.query;
   var method = req.method.toLowerCase();
 
-  //Get the headers 
-  var headers = req.headers
+  //Get the headers
+  var headers = req.headers;
   console.log(headers);
 
   //Get the payload if any
-  var decoder = new StringDecoder('utf-8')
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
 
-  //Get the response
-  res.end("Hello world\n");
+    //Get the response
+    res.end("Hello world\n");
 
-  //Log the request path
-  console.log(
-    "Request is received on path: ", queryStringObject
-  );
+    //Log the request path
+    console.log("Request is received on path: ", queryStringObject);
+    console.log("Request is received with payload: ", buffer);
+  });
 });
 
 //Start the server and let it listen on port 3000
