c8db7557de7b8fff8d0a12009057455a10bce39e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-07 01:35:33 +0100

    restructure files, and add initialize the server in the index file

--- index.js
+++ index.js
@@ -3,4 +3,18 @@
 //Dependencies
 
 const server = require('./lib/server');
-const worker = require('./lib/workers')
+const workers = require('./lib/workers');
+
+//Declare the application
+const app = {};
+
+app.init = () => {
+  //Start the server
+  server.init();
+  //Start the workers
+  workers.init();
+}
+
+app.init()
+
+module.exports = app;
