e6fcf19d0de8080f75c0d77d9b4e89188a7b0db5
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-12-20 17:01:56 +0100

    set up xmlhttprequest

--- public/app.js
+++ public/app.js
@@ -25,5 +25,51 @@ app.client.request = function(headers,path,method,queryStringObject,payload,call
     payload = typeof(payload) == 'object' && payload !== null ? payload : {}; payload
     callback = typeof(callback) == 'function' ? callback : false
 
+    //for each querystring parameter sent, add it to the path
+    let requestUrl = path+'?'
+    let counter = 0;
+    for (let queryKey in queryStringObject) {
+        if(queryStringObject.hasOwnProperty(queryKey)) {
+            counter += 1;
+            //if at least one parameter has already been added, prepend new ones with an ampersand
+            if(counter > 1){
+                requestUrl+='&'
+            }
+            //Add the key and value
+            requestUrl+=queryKey+'='+queryStringObject[queryKey]
+        }
+    }
+    //form the http request as a json format
+    const xhr = new XMLHttpRequest()
+    xhr.open(method,requestUrl,true)
+    xhr.setRequestHeader('Content-Type','application/json')
 
+    //for each header sent, add it to the request
+    for (const headerKey in headers){
+        if(headers.hasOwnProperty(headerKey)){
+            xhr.setRequestHeader(headerKey,headers[headerKey])
+        }
+    }
+    //if there is a current session token, add that as a header
+    if(app.config.sessionToken){
+        xhr.setRequestHeader('token',app.config.sessionToken.id)
+    }
+    //when the request comes back, handle the response
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            const statusCode = xhr.status
+            const responseReturned = xhr.responseText
+            //callback if requested
+            if(callback){
+                try {
+                    const parsedResponse = JSON.parse(responseReturned)
+                    callback(statusCode, parsedResponse)
+                } catch (e) {
+                    callback(statusCode,false)
+                }
+            }
+        }
+    }
+    const payloadString =  JSON.stringify(payload)
+    xhr.send(payloadString)
 }
