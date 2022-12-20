1e1372db7f88a60097fb45fdeab3aca0fde5f224
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-12-20 15:27:09 +0100

    check all parameters coming from the request function

--- public/app.js
+++ public/app.js
@@ -15,3 +15,15 @@ app.config = {
 app.client = {};
 
 //Interface for the API calls
+
+app.client.request = function(headers,path,method,queryStringObject,payload,callback){
+    //set default values for the parameters
+    headers = typeof(headers) == 'object' && headers !== 'null' ? headers : {};
+    path = typeof(path) == 'string' ? path : '/'
+    method = typeof(method) == 'string' && ['POST', 'PUT', 'DELETE','GET'].indexOf(method) > -1 ? method.toUpperCase() : 'GET'
+    queryStringObject = typeof(queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {}; queryStringObject
+    payload = typeof(payload) == 'object' && payload !== null ? payload : {}; payload
+    callback = typeof(callback) == 'function' ? callback : false
+
+
+}
