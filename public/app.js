c54455497cb80419c42387e78bb9155b2ee19b6d
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-12-20 14:47:53 +0100

    create client and config container

--- public/app.js
+++ public/app.js
@@ -2,7 +2,16 @@
 /*
 Frontend logic for the Application
 */
-
+//Container for the frontend application
 const app = {}
 
-console.log('Hello console world')
+
+app.config = {
+    'sessionToken': false
+}
+
+//AJAX client for the REST API
+
+app.client = {};
+
+//Interface for the API calls
