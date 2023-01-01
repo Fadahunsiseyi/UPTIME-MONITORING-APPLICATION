d16c33f9d2d6e90d1703f8e12a72346e258a7c53
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-01-01 18:29:49 +0100

    function to run tests

--- test/index.js
+++ test/index.js
@@ -49,6 +49,46 @@ _app.tests.unit['helpers.getANumber should return 2'] = (done)=>{
 //     return counter
 // }
 
+// _app.runTests = () =>{
+//     let errors = []
+//     let success = 0
+//     const limit = _app.countTests()
+//     let counter = 0
+//     for(let key in _app.tests){
+//         if(_app.tests.hasOwnProperty(key)){
+//             const subTests = _app.tests[key]
+//             for(let testName in subTests){
+//                 if(subTests.hasOwnProperty(testName)){
+//                     (()=>{
+//                         const tmpTestName = testName
+//                         const testValue = subTests[testName]
+//                         try {
+//                             testValue(()=>{
+//                                 console.log('\x1b[32m%s\x1b[0m', tmpTestName)
+//                                 counter += 1
+//                                 success += 1
+//                                 if(counter == limit){
+//                                     _app.produceTestReport(limit,success,errors)
+//                                 }
+//                             })
+//                         } catch (error) {
+//                             errors.push({
+//                                 'name': testName,
+//                                 'error':error
+//                             })
+//                             console.log('\x1b[31m%s\x1b[0m',tmpTestName)
+//                             counter += 1
+//                             if(counter == limit){
+//                                 _app.produceTestReport(limit,success,errors)
+//                             }
+//                         }
+//                     }) ()
+//                 }
+//             }
+//         }
+//     }
+//     return counter
+// }
 
 
 
