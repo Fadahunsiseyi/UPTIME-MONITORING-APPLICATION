35fba98ee467111d4d874b14f462877b149f81d8
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-01-02 23:18:09 +0100

    move the imported directories with the unit test to a new file

--- test/index.js
+++ test/index.js
@@ -1,9 +1,5 @@
 
 
-const helpers = require('./../lib/helpers')
-const assert = require('assert')
-const app = require('../index-debug')
-
 
 //Application for the test runner
 
@@ -12,27 +8,13 @@ _app ={}
 
 //Container for the tests
 
-_app.tests = {
-    'unit' : {}
-}
+_app.tests = {}
 
-_app.tests.unit['helpers.getANumber should return 1'] = (done)=>{
-    const val = helpers.getANumber()
-    assert.equal(val, 1)
-    done()
-}
 
-_app.tests.unit['helpers.getANumber should return number'] = (done)=>{
-    const val = helpers.getANumber()
-    assert.equal(typeof(val), 'number')
-    done()
-}
+//Add on the unit tests
 
-_app.tests.unit['helpers.getANumber should return 2'] = (done)=>{
-    const val = helpers.getANumber()
-    assert.equal(val, 2)
-    done()
-}
+_app.tests.unit = require('./unit')
+
 
 _app.countTests = () =>{
     let counter = 0
